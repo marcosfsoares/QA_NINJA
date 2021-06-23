c22fdbd60fdd7797d55abf33b8b8c74d6295880d
Marcos F. Soares <marcos.fsoares#yahoo.com>
2021-06-23 14:55:40 -0300

    Added example of "for" and "each" to verify variable usage outside the loop

--- A180/code4testers/8_loops.rb
+++ A180/code4testers/8_loops.rb
@@ -19,3 +19,28 @@ bandas.size.times do
     puts bandas[i]
     i += 1
 end
+
+
+# Utilizando a construção .each do
+bandas.each do |item|
+    puts item
+end
+
+lista = ['1','2','3','4']
+
+
+# Utilizando a construção for. A variável que percorre a lista
+# é acessível fora do escopo do comando for.
+for x in lista do
+    puts "Passei por aqui, e agora x é uma variável acessível fora do escopo do for, e pra provar isso vou imprimir x fora do for mesmo sem ter declarado a variável fora do escopo dele"
+end
+
+puts x
+
+lista.each do |y|
+    puts "Passei por aqui, e desta vez, y não é uma variável acessível fora do escopo do for, pq o .each isola ela. E pra provar isso vou tentar imprimir y fora do escopo do .each e vou tomar erro"
+end
+
+puts y
+
+
