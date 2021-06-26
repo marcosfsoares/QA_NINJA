74ee2a88f54c38fdc59e5bc25a4e2aeed239c49d
Marcos F. Soares <marcos.fsoares#yahoo.com>
2021-06-25 22:50:56 -0300

    Added new step definitions

--- A180/rocklov/features/cadastro.feature
+++ A180/rocklov/features/cadastro.feature
@@ -10,25 +10,29 @@ Funcionalidade: Cadastro
         Dado que acesso a página de cadastro
         Quando submeto o meu cadastro completo
         Então sou redirecionado para o Dashboard
-
+    
+    @tentativa_cadastro
     Cenario: Submeter cadastro sem o nome
 
         Dado que acesso a página de cadastro
         Quando submeto o meu cadastro sem o nome
         Então vejo a mensagem de alerta: Oops. Informe seu nome completo!
 
+    @tentativa_cadastro
     Cenario: Submeter cadastro sem o email
 
         Dado que acesso a página de cadastro
         Quando submeto o meu cadastro sem o email
         Então vejo a mensagem de alerta: Oops. Informe um email válido!
 
+    @tentativa_cadastro
     Cenario: Submeter cadastro com email incorreto
 
         Dado que acesso a página de cadastro
-        Quando submeto o meu cadastro com incorreto
+        Quando submeto o meu cadastro com o email incorreto
         Então vejo a mensagem de alerta: Oops. Informe um email válido!
 
+    @tentativa_cadastro
     Cenario: Submeter cadastro sem a senha
 
         Dado que acesso a página de cadastro
--- A180/rocklov/features/step_definitions/cadastro_steps.rb
+++ A180/rocklov/features/step_definitions/cadastro_steps.rb
@@ -9,37 +9,52 @@ Quando('submeto o meu cadastro completo') do
 
     find("#password").set "senha"
     click_button "Cadastrar"
-    sleep 10 #temporario
+    
 end
 
 Então('sou redirecionado para o Dashboard') do
-    expect(page).to have_css ".dashboard"    
+    expect(page).to have_css ".dashboard"
+        
 end
 
 Quando('submeto o meu cadastro sem o nome') do
-pending # Write code here that turns the phrase above into concrete actions
+    find("#email").set Faker::Internet.free_email #=> "freddy@gmail.com"
+    find("#password").set "senha"
+    click_button "Cadastrar"
 end
 
 Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
-pending # Write code here that turns the phrase above into concrete actions
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe seu nome completo!"
+    
 end
 
 Quando('submeto o meu cadastro sem o email') do
-pending # Write code here that turns the phrase above into concrete actions
-end
+    find("#fullName").set "Marcos Soares"
+    find("#password").set "senha"
+    click_button "Cadastrar"end
 
 Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do
-pending # Write code here that turns the phrase above into concrete actions
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe um email válido!"
+    
 end
 
-Quando('submeto o meu cadastro com incorreto') do
-pending # Write code here that turns the phrase above into concrete actions
+Quando('submeto o meu cadastro com o email incorreto') do
+    find("#fullName").set "Marcos Soares"
+    find("#email").set "email*incorreto.com"
+    find("#password").set "senha"
+    click_button "Cadastrar"
 end
 
 Quando('submeto o meu cadastro sem a senha') do
-pending # Write code here that turns the phrase above into concrete actions
+    find("#fullName").set "Marcos Soares"
+    find("#email").set Faker::Internet.free_email #=> "freddy@gmail.com"
+    click_button "Cadastrar"
 end
 
 Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
-pending # Write code here that turns the phrase above into concrete actions
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe sua senha secreta!"
+    
 end
