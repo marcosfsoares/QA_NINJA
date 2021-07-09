aecf1355eca0dcd82bb81348a933bc0d574a82a1
marcosfsoares <marcos.fsoares@yahoo.com>
2021-07-09 12:43:54 -0300

    FInished class about Classes and Inheritance in Ruby.

--- A180/code4testers/carro.rb
+++ A180/code4testers/carro.rb
@@ -1,5 +1,4 @@
-
-class Carro
+class Veiculo
     attr_accessor :nome, :cor, :marca
 
     def initialize(nome, marca)
@@ -8,8 +7,34 @@ class Carro
         @cor = "azul"
     end
 
+    def define_cor(cor)
+        @cor = cor
+    end
+
+end
+
+
+class Carro < Veiculo
+    
     def ligar
         puts "O #{@nome} está pronto para iniciar o trajeto"
+    end
+
+    def dirigir
+        puts "O #{@nome} está iniciando o trajeto"
+    end
+
+end
+
+
+class Moto < Veiculo
+    
+    def ligar
+        puts "A #{@nome} está pronta para iniciar o trajeto"
+    end
+
+    def pilotar
+        puts "A #{@nome} está iniciando o trajeto"
     end
 
 end
@@ -19,3 +44,16 @@ corcel2 = Carro.new("Corcel", "Ford")
 #corcel2.cor = "azul"
 #corcel2.marca = "Ford"
 corcel2.ligar
+corcel2.dirigir
+puts "A cor do #{corcel2.nome} é #{corcel2.cor}"
+
+fusca = Carro.new("Fusca", "VolksWagen")
+fusca.ligar
+fusca.dirigir
+fusca.define_cor("Preto")
+puts "A cor do #{fusca.nome} é #{fusca.cor}"
+cg = Moto.new("CG", "Honda")
+cg.ligar
+cg.pilotar
+cg.define_cor("Vermelha")
+puts "A cor da #{cg.nome} é #{cg.cor}"
